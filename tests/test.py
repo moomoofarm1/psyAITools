86200688a812445e8081409e33dc6aeec8d0c648
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-04-26 21:22:27 +0200

    Bugs fixed.

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
 from psyaitools.functional.IOs.base64_xfccs import base64_xfccs
 
-aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.csv', feature_set="mfcc")
-aaa = wav_xfccs('../../comDat/test')
+aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.wav', feature_set="mfcc")
+aaa = wav_xfccs('../../comDat/test.wav')
