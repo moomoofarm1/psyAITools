b786b5942288a01a12e86a8f5b470439f3a6a300
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-04-26 20:56:47 +0200

    Minor issues fixed

--- psyaitools/functional/IOs/base64_xfccs.py
+++ psyaitools/functional/IOs/base64_xfccs.py
@@ -6,7 +6,7 @@ from ..representation.xfccs import *
 
 def base64_xfccs(path, audioRow, fileName,feature_set="mfcc"):
     dat1 = getRecDat(path)
-    base64ToWebm(dat1.loc[audioRow,"response"], path, method=2)
+    base64ToWebm(dat1.loc[audioRow, "response"], fileName, method=2)
     webm2Wav(fileName)
     fs, sig = getWavDat(fileName + '.wav')
     out1, _ = xfccs(fs, sig, feature_set=feature_set)
