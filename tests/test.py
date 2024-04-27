1c017dce82dcfaef2fb66af6e8b28638a8f17562
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-04-27 13:41:51 +0200

    Added loudness.

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from psyaitools.functional.IOs.base64_xfccs import base64_xfccs
+from psyaitools.func.IOs.base64_xfccs import *
 
 aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.webm', feature_set="mfcc")
-#aaa = wav_xfccs('../../comDat/test.wav')
+aaa = wav_xfccs('../../comDat/test.wav')
