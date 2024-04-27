47e751a11b3f3abf183041c718c80a07b51fc124
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-04-27 13:59:38 +0200

    Checking loudness

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
 from psyaitools.func.IOs.base64_xfccs import *
+import soundfile as sf
 
-aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.webm', feature_set="mfcc")
-aaa = wav_xfccs('../../comDat/test.wav')
+# aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.webm', feature_set="mfcc")
+# aaa = wav_xfccs('../../comDat/test.wav')
+
+fs, sig = getWavDat('../../comDat/test.wav')
+print(type(sig))
+print(type(fs))
+aaa = getLoud('../../comDat/test.wav')
