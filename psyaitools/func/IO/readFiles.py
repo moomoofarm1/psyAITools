f9a8148c3f8007f9872e663d47cd9a0a0e34082c
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-05-09 13:32:50 +0200

    Package peft added.

--- psyaitools/func/IO/readFiles.py
+++ psyaitools/func/IO/readFiles.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
 from scipy.io.wavfile import read
 
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,6 @@
-from psyaitools.func.IOs.base64_xfccs import *
-import soundfile as sf
+from psyaitools.func.IO.base64_xfccs import *
 
 # aaa = base64_xfccs('../../comDat/testBase64.csv', 33, '../../comDat/test.webm', feature_set="mfcc")
 # aaa = wav_xfccs('../../comDat/test.wav')
-
-fs, sig = getWavDat('../../comDat/test.wav')
-print(type(sig))
-print(type(fs))
 aaa = getLoud('../../comDat/test.wav')
+print(aaa)
